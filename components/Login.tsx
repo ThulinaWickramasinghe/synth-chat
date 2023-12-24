222028bac9e7f75085a40779cee3268cbe5162b9
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-12-24 23:34:30 +0530

    Setup google oauth

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,6 +1,20 @@
+'use client'
+
+import { signIn } from "next-auth/react"
+import Image from "next/image"
+
 function Login() {
   return (
-    <div>Login</div>
+    <div className="bg-[#74AA9C] h-screen flex flex-col items-center justify-center text-center">
+        <Image
+        src="/chatgpt-logo.svg"
+        alt="ChaGPT logo"
+        width={125}
+        height={125}
+        
+        />
+        <button onClick={() => signIn('google')} className="text-white font-bold text-3xl animate-pulse py-10">Sign In to use ChatGPT</button>
+    </div>
   )
 }
 
