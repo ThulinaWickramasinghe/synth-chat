a6d5c1d0d7dde17f0d27a85f1f19d116fb751568
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-12-24 23:53:45 +0530

    Add user's profile picture to sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,12 @@
+"use client"
+
+import { useSession, signOut } from 'next-auth/react';
 import NewChat from './NewChat';
+import Image from 'next/image';
 
 function SideBar() {
+  const {data : session} = useSession()
+
   return (
     <div className='flex h-screen flex-col p-2'>
       <div className='flex-1'>
@@ -10,6 +16,16 @@ function SideBar() {
           {/* Map chats in chat history */}
         </div>
       </div>
+      {session && (
+      <Image
+        src={session.user?.image!}
+        alt="User's profile picture"
+        width={48}
+        height={48}
+        className='h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50'
+        onClick={() => signOut()}
+      />
+      )}
     </div>
   );
 }
