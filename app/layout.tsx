8e05b7d5c7c516e9362f86b9e98d2e053528e2d4
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-12-24 21:49:12 +0530

    Setup session

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,39 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import SideBar from '@/components/SideBar';
+import { SessionProvider } from '@/components/SessionProvider';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import Login from '@/components/Login';
 
 export const metadata: Metadata = {
   title: 'ChatGPT',
   description: 'A chat gpt clone',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions)
+
   return (
     <html lang='en'>
       <body>
-        <div className='flex'>
-          {/* Sidebar */}
-          {/* Client Provider  */}
-          <div className='flex-1 bg-[#343541]'>{children}</div>
-        </div>
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login/>
+          ): (
+            <div className='flex'>
+              <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
+                <SideBar/>
+              </div>
+              <div className='flex-1 bg-[#343541]'>{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
