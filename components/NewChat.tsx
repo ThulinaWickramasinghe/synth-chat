a71636e371997a1bb7f7cf4e92163d2e492bc10d
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-12-27 17:16:43 +0530

    Add functionality to New Chat button

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,8 +1,35 @@
+'use client';
+
+import { db } from '@/firebase';
 import { PlusIcon } from '@heroicons/react/24/solid';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 function NewChat() {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const createNewChat = async () => {
+    const doc = await addDoc(
+      collection(db, 'users', session?.user?.email!, 'chats'),
+      {
+        userId: session?.user?.email!,
+        createdAt: serverTimestamp(),
+      }
+    );
+
+    router.push(`/chat/${doc.id}`);
+  };
+
   return (
-    <div className='chatRow border border-gray-700'>
+    <div
+      onClick={createNewChat}
+      onKeyDown={(e) => e.key === 'Enter' && createNewChat()} // Handle Enter key press
+      role='button'
+      tabIndex={0}
+      className='chatRow border border-gray-700'
+    >
       <PlusIcon className='h-4 w-4' />
       <p>New Chat</p>
     </div>
