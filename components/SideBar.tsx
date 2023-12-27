5b2d9e8646ded1014e1098fd0a2c6be4fb1fece6
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-12-27 16:49:14 +0530

    Format code

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,11 +1,11 @@
-"use client"
+'use client';
 
 import { useSession, signOut } from 'next-auth/react';
 import NewChat from './NewChat';
 import Image from 'next/image';
 
 function SideBar() {
-  const {data : session} = useSession()
+  const { data: session } = useSession();
 
   return (
     <div className='flex h-screen flex-col p-2'>
@@ -17,14 +17,14 @@ function SideBar() {
         </div>
       </div>
       {session && (
-      <Image
-        src={session.user?.image!}
-        alt="User's profile picture"
-        width={48}
-        height={48}
-        className='h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50'
-        onClick={() => signOut()}
-      />
+        <Image
+          src={session.user?.image!}
+          alt="User's profile picture"
+          width={48}
+          height={48}
+          className='mx-auto mb-2 h-12 w-12 cursor-pointer rounded-full hover:opacity-50'
+          onClick={() => signOut()}
+        />
       )}
     </div>
   );
