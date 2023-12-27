b823865a2165452b1aa33ef916ca6ffb834029a5
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-12-27 16:47:57 +0530

    Setup firebase firestore

---
+++ firebase.ts
@@ -0,0 +1,18 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+//Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_NeiNyAXk5oMT2DL9-lEmAAiTyF24bZA",
+  authDomain: "synth-chat-3cd16.firebaseapp.com",
+  projectId: "synth-chat-3cd16",
+  storageBucket: "synth-chat-3cd16.appspot.com",
+  messagingSenderId: "733896687204",
+  appId: "1:733896687204:web:c8576e383015f60bdca971"
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export {db}
