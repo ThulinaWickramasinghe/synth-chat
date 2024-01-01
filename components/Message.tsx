dd92693fcc12e11335d7eecedcfb8e4809f0c8f5
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2024-01-01 19:39:28 +0530

    Display messages in the chat

---
+++ components/Message.tsx
@@ -0,0 +1,26 @@
+import { DocumentData } from 'firebase/firestore';
+import Image from 'next/image';
+
+type Props = {
+  message: DocumentData;
+};
+
+function Message({ message }: Props) {
+  const isAI = message.user.name === 'ChatGPT';
+  return (
+    <div className={`py-5 text-white ${isAI && 'bg-[#434654]'}`}>
+      <div className='mx-auto flex max-w-2xl space-x-5 px-10'>
+        <Image
+          src={message.user.avatar}
+          className='h-8 w-8 rounded-lg'
+          alt="User's avatar"
+          width={32}
+          height={32}
+        />
+        <p className='pt-1 text-sm'>{message.text}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Message;
